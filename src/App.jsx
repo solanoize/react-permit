378f8b3cc4b73cf35faac7b6d35a322aecd03a22
Yanzen <yanwarsolah@gmail.com>
2024-06-20 19:00:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import useAuth from "./utils/hooks/useAuth.jsx";
+import { UtilStateContextBase } from "./utils/states/contexts";
+import {HashRouter, Route, Routes} from "react-router-dom";
+import PermissionSettingRouter from "./permissions/settings/PermissionSettingRouter.jsx";
+import RoleSettingRouter from "./roles/setttings/RoleSettingRouter.jsx";
+import UserSettingRouter from "./users/settings/UserSettingRouter.jsx";
+import DashboardSettingRouter from "./dashboards/settings/DashboardSettingRouter.jsx";
+
+function App() {
+  const auth = useAuth();
+
+  return (
+    <UtilStateContextBase.Provider value={{ auth }}>
+      <HashRouter>
+        <Routes>
+          <Route path={"/*"} element={<DashboardSettingRouter />} />
+          <Route path={"/permissions/*"} element={<PermissionSettingRouter />} />
+          <Route path={"/roles/*"} element={<RoleSettingRouter />} />
+          <Route path={"/users/*"} element={<UserSettingRouter />} />
+        </Routes>
+      </HashRouter>
+    </UtilStateContextBase.Provider>
+  );
+}
+
+export default App;
---
+++ src/managers/navigations/ManagerNavigationBase.jsx
@@ -0,0 +1,37 @@
+import { Container, Nav, NavDropdown } from "react-bootstrap";
+
+import Navbar from "react-bootstrap/Navbar";
+import {useContext} from "react";
+import {UtilStateContextBase} from "../../utils/states/contexts.jsx";
+import PermissionSettingMenu from "../../permissions/settings/PermissionSettingMenu.jsx";
+import RoleSettingMenu from "../../roles/setttings/RoleSettingMenu.jsx";
+import UserSettingMenu from "../../users/settings/UserSettingMenu.jsx";
+
+function ManagerNavigationBase() {
+  const context = useContext(UtilStateContextBase);
+
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">POS Lite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#/">Dashboard</Nav.Link>
+              <PermissionSettingMenu />
+              <RoleSettingMenu />
+              <UserSettingMenu />
+              <NavDropdown title="Settings" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={() => {
+                  context.auth.signOut();
+                }}>Sign Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default ManagerNavigationBase;
+
